3bdf7da94cf372271915948f05aacb8f6d65470e
Na Hyung Choi <schoinh@gmail.com>
2019-08-02 11:37:39 -0700

    add home index

--- VendorOrderTracker/Controllers/HomeController.cs
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -5,10 +5,10 @@ namespace VendorOrderTracker.Controllers
 {
     public class HomeController : Controller
     {
-        // [HttpGet("/")]
-        // public ActionResult Index()
-        // {
-        //     return View();
-        // }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
