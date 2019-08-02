3905ce4764088095e97222efb9f432433979ecf5
Na Hyung Choi <schoinh@gmail.com>
2019-08-02 09:28:48 -0700

    initial commit

---
+++ VendorOrderTracker.Tests/Models/OrderTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+    [TestClass]
+    public class OrderTest
+    {
+        // [TestMethod]
+        // public void MethodName_Spec_ExpectedOutput()
+        // {
+        //     Order instanceName = new Order();
+        //     Assert.AreEqual(ExpectedOutput, instanceName.MethodName(inputHere));
+        // }
+    }
+}
