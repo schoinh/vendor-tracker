14351b833f192cf4d3bbba31fb2aa04ac9ead404
Na Hyung Choi <schoinh@gmail.com>
2019-08-02 09:28:08 -0700

    create Order class

---
+++ VendorOrderTracker/Models/Order.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+    public class Order
+    {
+        // public string Name { get; set; }
+        // public string Description { get; set; }
+        // public List<Order> Orders { get; set; }
+        // public int Id { get; set; }
+        // private static int _idCount = 1;
+
+        // public Vendor(string name, string description)
+        // {
+        //     Name = name;
+        //     Description = description;
+        //     Orders = new List<Order>() {};
+        //     Id = _idCount;
+        //     _idCount++;
+        // }
+
+
+    }
+}
