0c272dd24627641a2d9fbb39cb899fc236b22326
Na Hyung Choi <schoinh@gmail.com>
2019-08-02 11:38:04 -0700

    add routes for viewing, adding, and finding vendors

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -6,37 +6,38 @@ namespace VendorOrderTracker.Controllers
 {
     public class VendorsController : Controller
     {
-        // [HttpGet("/vendors")]
-        // public ActionResult Index()
-        // {
-        //     // List<Item> allItems = Item.GetAll();
-        //     // return View(allItems);
-        // }
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
 
-        // [HttpGet("/vendors/new")]
-        // public ActionResult New()
-        // {
-        //     return View();
-        // }
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
 
-        // [HttpPost("/vendors")]
-        // public ActionResult Create(string description)
-        // {
-        //     // Item myItem = new Item(description);
-        //     return RedirectToAction("Index");
-        // }
+        [HttpPost("/vendors")]
+        public ActionResult Create(string name, string description)
+        {
+            Vendor newVendor = new Vendor(name, description);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Vendor.Find(id);
+            return View(Vendor.FoundVendor);
+        }
 
         // [HttpPost("/vendors/delete")]
         // public ActionResult DeleteAll()
         // {
-        //     // Item.ClearAll();
+        //     // Vendor.ClearAll();
         //     return View();
-        // }
-        // [HttpGet("/vendors/{id}")]
-        // public ActionResult Show(int id)
-        // {
-        //     // Item foundItem = Item.Find(id);
-        //     // return View(foundItem);
         // }
     }
 }
